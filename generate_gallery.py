e02c48292f4595dc1a319d881ce0c048dc36a3c9
michaelwoodc <michaelwoodcock@hotmail.com>
2023-12-12 17:11:27 -0500

    almost done!

---
+++ generate_gallery.py
@@ -0,0 +1,24 @@
+def generate_gallery_html(start_number, end_number, directory):
+    html_template = """
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="{img_path}">
+      <img src="{img_path}" alt="Image {img_number}">
+    </a>
+    <div class="desc"> Insert description here </div>
+  </div>
+</div>
+"""
+
+    for img_number in range(start_number, end_number + 1):
+        img_path = f"{directory}\\{img_number:02d}.jpeg"
+        print(html_template.format(img_number=img_number, img_path=img_path))
+
+
+# Set your desired range and directory
+start_number = 1
+end_number = 26
+image_directory = "media\\akeebabackup"
+
+# Generate and print the HTML code
+generate_gallery_html(start_number, end_number, image_directory)
--- navbar.js
+++ navbar.js
@@ -20,3 +20,26 @@ document.addEventListener('DOMContentLoaded', function () {
     // Send the request
     xhr.send();
 });
+
+// Function to copy code to clipboard
+function copyCodeToClipboard() {
+    // Get the code block element
+    var codeBlock = document.getElementById("codeBlock");
+    // Create a range and select the code
+    var range = document.createRange();
+    range.selectNode(codeBlock);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    // Execute the copy command
+    document.execCommand("copy");
+    // Deselect the text
+    window.getSelection().removeAllRanges();
+    // Update button text
+    var copyButton = document.getElementById("copyButton");
+    copyButton.innerText = "Code Copied!";
+    setTimeout(function() {
+        copyButton.innerText = "Copy Code";
+    }, 2000); // Reset button text after 2 seconds
+}
+// Add click event listener to the copy button
+document.getElementById("copyButton").addEventListener("click", copyCodeToClipboard);
